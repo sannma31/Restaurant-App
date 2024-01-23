14059bb2fb285b56e4e047f42176af7d67a152bb
sannma31 <kasaisyouun2004@gmail.com>
2024-01-23 15:36:37 +0900

    Initial Commit

---
+++ Restaurant App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant App
+//
+//  Created by 笠井翔雲 on 2024/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
