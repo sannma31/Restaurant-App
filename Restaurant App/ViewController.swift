8ac658a969d55e1860b4d1bcccec03b9412cbca3
sannma31 <kasaisyouun2004@gmail.com>
2024-01-26 16:39:36 +0900

    add:MapViewの追加

--- Restaurant App/ViewController.swift
+++ Restaurant App/ViewController.swift
@@ -4,37 +4,54 @@
 //
 //  Created by 笠井翔雲 on 2024/01/23.
 //
-
 import UIKit
 import CoreLocation
+import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
-
+class ViewController: UIViewController {
+    @IBOutlet weak var Map: MKMapView!
     let locationManager = CLLocationManager()
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            locationManager.delegate = self // CLLocationManagerDelegateプロトコルを使用するためにdelegateをViewControllerクラスに設定する。
-            locationManager.requestWhenInUseAuthorization() // 位置情報の許可設定を通知する。
-            locationManager.requestLocation() // 1度だけ位置情報取得のリクエストを投げる。
-        }
-    
 
-    @IBAction func tappbutton(_ sender: Any) {
-        locationManager.requestLocation()
-        print("タップされました")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupLocationManager()
     }
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            if let location = locations.last {
-                print("location: \(location)") // CLLocationManagerクラスで取得した位置情報
-                print("緯度: \(location.coordinate.latitude)")
-                print("経度: \(location.coordinate.longitude)")
-            }
-        }
-
-        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-            print("error: \(error)")
-        }
-    
 }
 
+extension ViewController: CLLocationManagerDelegate {
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+
+    func startUpdatingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("location: \(location)")
+            print("緯度: \(location.coordinate.latitude)")
+            print("経度: \(location.coordinate.longitude)")
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("location manager failed with error: \(error.localizedDescription)")
+        // ここでエラーに対する適切な処理を追加する
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            startUpdatingLocation()
+        default:
+            break
+        }
+    }
+}
